40ffd512606e2c6b18eca120d03607ca15718ce1
carlcst <Carl.stagg@stagg.org>
2020-01-26 15:33:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-let strip = light.createStrip(pins.D9, 30)
+let strip = light.createStrip(pins.D13, 150)
+strip.setAll(0x0000ff)
 forever(function () {
-    strip.showAnimation(light.rainbowAnimation, 200)
+    strip.showAnimation(light.rainbowAnimation, 5000)
+    strip.showAnimation(light.cometAnimation, 5000)
+    strip.showAnimation(light.theaterChaseAnimation, 5000)
 })
