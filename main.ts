ca066c9e6cb4920d2e2f50d998146ead54b75b71
carlcst <Carl.stagg@stagg.org>
2020-01-26 16:09:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,12 @@ let strip = light.createStrip(pins.D13, 150)
 strip.setAll(0x0000ff)
 forever(function () {
     if (input.touchA5.isPressed()) {
+        strip.setAll(0xff0000)
+    }
+    if (input.touchA4.isPressed()) {
+        strip.setAll(0x00ff00)
+    }
+    if (input.touchA3.isPressed()) {
         strip.setAll(0x0000ff)
-    } else {
-        strip.showAnimation(light.runningLightsAnimation, 1000)
     }
 })
